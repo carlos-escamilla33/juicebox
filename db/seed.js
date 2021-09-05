802848dbbaffd052aa224ab6d44fcc70a8ad79cf
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-04 23:25:45 -0700

    created table for database

--- db/seed.js
+++ db/seed.js
@@ -9,9 +9,9 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create users...");
 
-        const albert = await createUser({ username: 'albert', password: 'bertie99' });
-
-        console.log(albert);
+        await createUser({ username: 'albert', password: 'bertie99' });
+        await createUser({ username: 'sandra', password: '2sandy4me' });
+        await createUser({ username: 'glamgal', password: 'soglam' });
 
         console.log("Finished creating users!");
     } catch (error) {
@@ -57,18 +57,6 @@ async function createTables() {
     }
 }
 
-async function rebuildDB() {
-    try {
-        client.connect();
-
-        await dropTables();
-        await createTables();
-        await createInitialUsers();
-    } catch (error) {
-        throw error;
-    }
-}
-
 async function testDB() {
     try {
         console.log("Starting to test database...");
@@ -79,6 +67,18 @@ async function testDB() {
         console.log("Finished database tests!");
     } catch (error) {
         console.error("Error testing database!");
+        throw error;
+    }
+}
+
+async function rebuildDB() {
+    try {
+        client.connect();
+
+        await dropTables();
+        await createTables();
+        await createInitialUsers();
+    } catch (error) {
         throw error;
     }
 }
