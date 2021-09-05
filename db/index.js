e2da97b9a2a49c19347df753879f3a1c339e627d
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-04 20:00:14 -0700

    working on Testing, Wrap Up in workshop

---
+++ db/index.js
@@ -0,0 +1,29 @@
+const { Client } = require("pg");
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+const getAllUsers = async () => {
+    const { rows } = await client.query(`
+        SELECT id, username
+        FROM users;
+    `);
+    return rows
+}
+
+async function createUser({ username, password }) {
+    try {
+      const result = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2);
+      `, [username, password]);
+  
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser,
+}
