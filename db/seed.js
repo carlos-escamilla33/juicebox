58e771799957cd9af4b8e263ff21b19b6f371128
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-08 20:03:36 -0700

    added name, location values

--- db/seed.js
+++ db/seed.js
@@ -4,14 +4,32 @@ const {
     createUser,
 } = require('./index');
 
-// new function, should attempt to create a few users
-const createInitialUsers = async() => {
+const createInitialUsers = async () => {
     try {
         console.log("Starting to create users...");
 
-        await createUser({ username: 'albert', password: 'bertie99' });
-        await createUser({ username: 'sandra', password: '2sandy4me' });
-        await createUser({ username: 'glamgal', password: 'soglam' });
+        await createUser({
+            username: 'albert',
+            password: 'bertie99',
+            name: 'Al Bert',
+            location: 'Sidney, Australia',
+            active: true
+        });
+        await createUser({
+            username: 'sandra',
+            password: '2sandy4me',
+            name: "Al Bert",
+            location: 'Sidney, Australia',
+            active: true
+
+        });
+        await createUser({
+            username: 'glamgal',
+            password: 'soglam',
+            name: 'Joshua',
+            location: 'Upper East Side',
+            active: true
+        });
 
         console.log("Finished creating users!");
     } catch (error) {
@@ -19,8 +37,6 @@ const createInitialUsers = async() => {
         throw error;
     }
 }
-
-// then modify rebuildDB to call our new function
 
 
 const dropTables = async () => {
@@ -45,8 +61,11 @@ const createTables = async () => {
         await client.query(`
         CREATE TABLE users (
           id SERIAL PRIMARY KEY,
-          username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          location VARCHAR(255) NOT NULL,
+          active BOOLEAN DEFAULT true
         );
       `);
 
