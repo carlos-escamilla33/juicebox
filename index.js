31bdbc433bdf670c65730a3b2c02d59f3ebc3c0a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-13 21:32:13 -0700

    left off on routes part1 in juicebox part2

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const PORT = 3000;
 const express = require("express");
 const server = express();
+const apiRouter = require("./api");
+const morgan = require("morgan");
+
+server.use(express.json());
+
+server.use(morgan("dev"));
 
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>")
@@ -10,6 +16,8 @@ server.use((req, res, next) => {
 
     next();
 })
+
+server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
     console.log(`The server is up on port ${PORT}`)
