c35b0894b0fb6c28e074ef8a62755e95df19ef98
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-14 20:33:50 -0700

    working on api/tags.js

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ const express = require("express");
 const server = express();
 const apiRouter = require("./api");
 const morgan = require("morgan");
+const {client} = require ("./db");
 
 server.use(express.json());
 
 server.use(morgan("dev"));
 
-server.use((req, res, next) => {
-    console.log("<____Body Logger START____>")
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
+// server.use((req, res, next) => {
+//     console.log("<____Body Logger START____>")
+//     console.log(req.body);
+//     console.log("<_____Body Logger END_____>");
 
-    next();
-})
+//     next();
+// })
 
 server.use("/api", apiRouter);
-
-const {client} = require ("./db");
 client.connect();
 
 server.listen(PORT, () => {
