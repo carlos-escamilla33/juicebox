51c8454c90dfb380e0eca8cce0562a30428f11c6
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-18 14:02:22 -0700

    postsRouter.posts works

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require("../db")
+const { getAllPosts, createPost } = require("../db");
+const { requireUser } = require("./utils");
 
 
 postsRouter.use((req, res, next) => {
@@ -15,6 +16,35 @@ postsRouter.get("/", async (req, res) => {
     res.send({
         posts
     })
-})
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/);
+    const authorId = req.user.id;
+    const postData = { authorId, title, content, tags };
+
+    // only send the tags if there are some to send
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try {
+        const post = await createPost(postData);
+
+        if (post) {
+            res.send({ post });
+        } else {
+            next({
+                name: "postErrorMessage",
+                message: "This is an error message for posts"
+            })
+        }
+    }
+    catch ({ name, message }) {
+        next({ name, message })
+    }
+});
 
 module.exports = postsRouter;
