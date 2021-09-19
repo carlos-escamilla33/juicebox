a4e6160d244e5af7930b488685c973513c9950b7
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-19 00:08:01 -0700

    delete route works

--- api/posts.js
+++ api/posts.js
@@ -79,6 +79,30 @@ postsRouter.patch("/:postId", requireUser, async (req, res, next) => {
     catch ({ name, message }) {
         next({ name, message });
     }
+});
+
+postsRouter.delete("/:postId", requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    try {
+        const post = await getPostById(postId);
+
+        if (post && post.author.id === req.user.id) {
+            const updatedPost = await updatePost(post.id, { active: false });
+
+            res.send({ post: updatedPost })
+        } else {
+            next(post ? {
+                name: "UnauthorizedUserError",
+                message: "You cannot delete a post which is not yours"
+            } : {
+                name: "PostNotFoundError",
+                message: "That post does not exist"
+            })
+        }
+    }
+    catch ({ name, message }) {
+        next({ name, message })
+    }
 })
 
 module.exports = postsRouter;
