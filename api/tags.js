3712ddfa32802b53a4c20097a0ac08dd3e3dbf25
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-18 23:40:21 -0700

    tagsRouter for :tagName might need refactoring

--- api/tags.js
+++ api/tags.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
-tagsRouter.use((req,res,next) => {
+tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags")
 
     next();
@@ -14,6 +14,25 @@ tagsRouter.get("/", async (req, res) => {
     res.send({
         tags
     });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+    const tagName = req.params.tagName;
+    try {
+        const posts = await getPostsByTagName(tagName);
+        if (posts.length > 0) {
+            res.send({ posts })
+        } else {
+            next({
+                name: "TagName Error",
+                message: `Error, could not get tagName ${tagName}`
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        next(error)
+    }
 });
 
 module.exports = tagsRouter;
